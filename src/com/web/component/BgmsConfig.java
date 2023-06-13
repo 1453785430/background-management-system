ebd9390185babc0812a895fc8ca06be396918d48
JLuoyi <malegodj@163.com>
2023-06-13 19:40:00 +0800

    新增test

--- src/com/web/component/BgmsConfig.java
+++ src/com/web/component/BgmsConfig.java
@@ -10,4 +10,5 @@ public class BgmsConfig {
     public static final String ROLE_LIST = "roleList";
     public static final String USER_LIST = "userList";
     public static final String BUSINESS_LIST = "businessList";
+    public static final String TEST_GIT = "testGit";
 }
